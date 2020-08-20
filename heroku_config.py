f0f368dd9bccec7226a31bab8091db90d322099b
itsrxns <51463109+itsrxns@users.noreply.github.com>
2020-08-20 23:07:03 +0200

    Update heroku_config.py

--- heroku_config.py
+++ heroku_config.py
@@ -1,17 +1,17 @@
 import os
 
 class Var(object):
-    APP_ID = int(os.environ.get("APP_ID", 6))
+    APP_ID = int(os.environ.get("APP_ID", 1516656))
     # 6 is a placeholder
-    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    API_HASH = os.environ.get("API_HASH", "d9e0d8d4268862852c7ca848de66ccc7")
+    STRING_SESSION = os.environ.get("STRING_SESSION", 1BJWap1sBux_6lv3sq5aqcl54L2quFJs79p-1XnC2qncRUsojk_8zE3UAeP1qEuv2Eiwn9PA1IGWenOQSL9_cZDQmNVZYt6nYqaJrfCz9O_VRndXiBs6vnw5L2SFVy5WiR8O2JRu7hrfBTwXBb6BACk5WHo_nb-TK4A4Xqva7kHg-9wpyxhWpcDIFJ8aGqU7TAnJsO1959-deKdXAJTN_fIJ0T8Dc5En0_swaSN-hRRxgylXSGKB2CmfUR0EycaazpgzQ_bdmAHHrfUECY90zgVWjGXnDyT3to7S5QQF1mUeg0V0xXxujYeJig3GQh6fMQeJcTgCQovP-Q2Po22ZJRCli7Xq3rVI=)
     DB_URI = os.environ.get("DATABASE_URL", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", None)
     LOGGER = True
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
     # Here for later purposes
-    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "967883138").split())
+    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "1306336056").split())
     LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
     LESS_SPAMMY = os.environ.get("LESS_SPAMMY", None)
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
