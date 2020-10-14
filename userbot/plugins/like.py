023bc3073743258024944a1499e5616e79774140
itsrxns <51463109+itsrxns@users.noreply.github.com>
2020-10-14 05:01:29 +0200

    Create like.py

---
+++ userbot/plugins/like.py
@@ -0,0 +1,39 @@
+# Copyright © 2020 di 100101110 Github, <https://github.com/100101110>.
+#
+# Questo file fa parte del progetto <https://github.com/100101110/userbot-100101110>,
+# e viene rilasciato in base alla "Licenza GNU Affero General Public v3.0".
+# Si prega di consultare <https://github.com/100101110/userbot-100101110/blob/master/LICENSE>
+#
+# Tutti i diritti riservati.
+# 
+# Crediti: @100101110
+#
+"""Commands:
+.like
+.unlike"""
+
+import asyncio
+import random
+import logging
+
+from collections import deque
+from telethon import events
+from telethon.errors.rpcerrorlist import MessageIdInvalidError
+from userbot import bot
+from userbot.system import dev_cmd
+
+
+@bot.on(dev_cmd("like ?(.*)", outgoing=True))
+async def like(event):
+    await event.edit("**👍🏻 a** [Gucci RxnS 4L #VVS 💎⌚🇮🇹](t.me/lordrxns) **piace questo elemento.**")
+    
+
+
+@bot.on(dev_cmd("dislike ?(.*)", outgoing=True))
+async def dislike(event):
+    await event.edit("**👎🏻 a** [Gucci RxnS 4L #VVS 💎⌚🇮🇹](t.me/lordrxns) **non piace questo elemento.**")
+
+
+@bot.on(dev_cmd("unlike ?(.*)", outgoing=True))
+async def unslike(event):
+    await event.edit("**👎🏻 a** [Gucci RxnS 4L #VVS 💎⌚🇮🇹](t.me/lordrxns) **non piace questo elemento.**")
