7d56f8c06ba860c2691b57947865e0b5d1322d0b
itsrxns <51463109+itsrxns@users.noreply.github.com>
2020-10-14 07:21:43 +0200

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -73,7 +73,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             reply_pop_up_alert = "{} is useless".format(plugin_name)
         else:
             reply_pop_up_alert = help_string
-        reply_pop_up_alert += "\n Usa .unload {} per rimuovere il plugin\n\
+        reply_pop_up_alert += "\nℹ Usa .unload {} per rimuovere il plugin\n\
             © Userbot".format(plugin_name)
         try:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
