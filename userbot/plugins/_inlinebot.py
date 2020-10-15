d009ddf256d1e6d7535e062501868782f1d4861f
itsrxns <51463109+itsrxns@users.noreply.github.com>
2020-10-15 05:39:26 +0200

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -18,7 +18,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "© Userbot Help",
-                text="{}\nPlugins: {}".format(
+                text="{}\n**🔎 Comandi: {}**".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False
