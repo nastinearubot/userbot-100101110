967a483dd702da670c8f64dab1249df84c9e9df5
itsrxns <51463109+itsrxns@users.noreply.github.com>
2020-10-15 04:17:13 +0200

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -43,8 +43,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " plugin non valido!")
         else:
-            help_string = f"""**🤖 BOT** **helper di** {DEFAULTUSER}\n
-`Helper per i comandi`\nPremi .help plugin_name se non è qui."""
+            help_string = f"""**🤖 BOT:** **Ciao** [Gucci RxnS 4L #VVS 💎⌚🇮🇹](t.me/lordrxns), sono il tuo BOT helper\n
+*Qui troverai una lista di comandi utilizzabili e alcune informazioni:*"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
